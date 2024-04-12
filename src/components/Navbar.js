ea4fb9cda9799dcd805da8e089d785badee60052
Ales <aleskruba@seznam.cz>
2024-04-12 21:13:32 +0200

    about III

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,11 +12,15 @@ function Navbar() {
     <nav className="navbar">
         <div className="navbar-left">
         <a href="#" className="navbar-brand">Spanish with <span className='navbar-brand-name'>Dani</span></a>
+          <div className='navbar-image-div'>
+              <img src="girl.png" alt="Daniela"  className='navbar-image' />
+          </div>
+
         </div>
         <div className="navbar-right">
-        <a href="#" className="nav-link">About Me</a>
-        <a href="#" className="nav-link">Experience</a>
-        <a href="#" className="nav-link">Courses</a>
+          <a href="#" className="nav-link">About Me</a>
+          <a href="#" className="nav-link">Experience</a>
+          <a href="#" className="nav-link">Courses</a>
         <div className='switch'>
         <IoMdColorWand className='colorwand'/> 
             <ReactSwitch 
